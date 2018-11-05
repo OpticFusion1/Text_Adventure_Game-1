31d33b895d49a3ce98c90321bc477d16d9d774db
catw101 <catelenwu@gmail.com>
2018-11-04 21:22:30 -0500

    update

--- src/Creatures.java
+++ src/Creatures.java
@@ -2,13 +2,11 @@ public abstract class Creatures {
     private String type, name;
     private int level;
 
-    public Creatures(String type, String name, int level) {
+    public Creatures(String type) {
         this.type = type;
-        this.name = name;
-        this.level = level;
     }
 
     public String toString() {
-        return "I am a " + type + "pocket creature. \nMy name is " + name + ".";
+        return "I am a " + type + "pocket creature.";
     }
 }
