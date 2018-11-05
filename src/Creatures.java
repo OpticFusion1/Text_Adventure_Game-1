404c157b7b682cc234b0f349f3e4ffe740a8c841
catw101 <catelenwu@gmail.com>
2018-11-04 20:34:54 -0500

    update

--- src/Creatures.java
+++ src/Creatures.java
@@ -1,2 +1,11 @@
 public abstract class Creatures {
+    private String type;
+
+    public Creatures(String type) {
+        this.type = type;
+    }
+
+    public String toString() {
+        return "I am a " + type + "pocket creature.";
+    }
 }
