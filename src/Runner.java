e9ecde1eba1142dd16ed373235ebf6fa86afa8fd
BT_1N3_27 <cwu8959@bths.edu>
2018-11-05 11:04:35 -0500

    update

--- src/Runner.java
+++ src/Runner.java
@@ -6,19 +6,18 @@ public class Runner {
     private static boolean gameOn = true;
 
     public static String[] firstNames = {"Ashley", "Ben", "Chris", "David", "Evelyn", "Franny", "Gabe", "Hayley", "Ina", "Janet", "Kevin", "Larry"};
+    private static Person player1 = new Person(0,0,10,1);
 
     public static void main(String[] args) {
         //Room[][] building = new Room[10][10];
         //String[][] area = new String[10][20];
         //Room[][] house = new Room[10][10];
 
-        Board house = new Board(10,20);
+        Board house = new Board(10, 20);
 
-        for (int x = 0; x < house.board.length; x++)
-        {
-            for (int y = 0; y < house.board[x].length; y++)
-            {
-                house.board[x][y] = new Room(x,y);
+        for (int x = 0; x < house.board.length; x++) {
+            for (int y = 0; y < house.board[x].length; y++) {
+                house.board[x][y] = new Room(x, y);
             }
         }
 
@@ -31,21 +30,80 @@ public class Runner {
         //area[0][0].enterRoom(player1);
         Scanner in = new Scanner(System.in);
         System.out.println("You are currently in the house. Be on the lookout for some pocket creatures!");
-        System.out.println("You can move using W for North, A for West, S for South, and D for East.");
+        System.out.println("You can move using N for North, W for West, S for South, and E for East.");
         while (gameOn) {
+            System.out.println("Where would you like to move? (Choose N, S, E, W)");
             String move = in.nextLine();
-            if (move.equals("W")) {
-
-            }
-           /*if(validMove(move, player1, area))
-           {
+            if (validMove(move, player1, house.board)) {
                 System.out.println("Your coordinates: row = " + player1.getxLoc() + " col = " + player1.getyLoc());
-           }
-           else {
-                System.out.println("Please choose a valid move.");*/
+
+            } else {
+                System.out.println("Please choose a valid move.");
+            }
+
+
         }
-
-
+        in.close();
     }
-    //in.close();
+
+    public static boolean validMove(String move, Person p, Room[][] map)
+    {
+        move = move.toLowerCase().trim();
+        switch (move) {
+            case "n":
+                if (p.getxLoc() > 0)
+                {
+                    map[p.getxLoc()][p.getyLoc()].leaveRoom(p);
+                    map[p.getxLoc()-1][p.getyLoc()].enterRoom(p);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            case "e":
+                if (p.getyLoc()< map[p.getyLoc()].length -1)
+                {
+                    map[p.getxLoc()][p.getyLoc()].leaveRoom(p);
+                    map[p.getxLoc()][p.getyLoc() + 1].enterRoom(p);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            case "s":
+                if (p.getxLoc() < map.length - 1)
+                {
+                    map[p.getxLoc()][p.getyLoc()].leaveRoom(p);
+                    map[p.getxLoc()+1][p.getyLoc()].enterRoom(p);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            case "w":
+                if (p.getyLoc() > 0)
+                {
+                    map[p.getxLoc()][p.getyLoc()].leaveRoom(p);
+                    map[p.getxLoc()][p.getyLoc()-1].enterRoom(p);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            default:
+                break;
+
+        }
+        return true;
+    }
+    public static void gameOff()
+    {
+        gameOn = false;
+    }
 }
